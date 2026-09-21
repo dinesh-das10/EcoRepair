5cbc30fbab39c059e43fff22052707455280fa62
dinesh-das10 <dinesh5das10@gmail.com>
2026-09-21 20:44:41 +0530

    Allow Vercel frontend in CORS

--- backend/settings.py
+++ backend/settings.py
@@ -133,7 +133,7 @@ MAILERS = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://ecorepair-frontend.onrender.com",
+     "https://eco-repair.vercel.app",
 ]
 
 STATIC_URL = "static/"
